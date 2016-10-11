2ef89d9b219b97fe369839998a8709c1b3151000
lopoly <lopoly@gmail.com>
2016-10-11 10:17:34 +0300

    Chapter 13

--- app/src/main/java/ua/lopoly/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/ua/lopoly/criminalintent/SingleFragmentActivity.java
@@ -5,12 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by lopoly on 07.10.2016.
  */
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
